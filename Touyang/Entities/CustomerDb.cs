08cf9476dcbe93bb6ed4e838905c1342ffa26079
Vueyang <109055303+Vueyang@users.noreply.github.com>
2022-07-12 15:18:56 +0700

    Add Service API

---
+++ Touyang/Entities/CustomerDb.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Touyang.Entities
+{
+    public class CustomerDb
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public List<Datum> data { get; set; } = new();
+    }
+
+    public class Datum
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public int age { get; set; }
+    }
+
+
+}
---
+++ Touyang/Entities/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Touyang.Entities
+{
+    public class ResponseModel
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
